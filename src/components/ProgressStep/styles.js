f381dc107d05ca923499c898ef330c1d390abdec
gustavool <gustavool@live.com>
2022-03-13 19:14:23 -0300

    progress bar style refactored

--- src/components/ProgressStep/styles.js
+++ src/components/ProgressStep/styles.js
@@ -7,38 +7,13 @@ export const Container = styled.div`
   text-align: center;
   font-size: 16px;
 
-  #firstBar {
-    border-top-left-radius: 4px;
-    border-bottom-left-radius: 4px;
-    border-left: 3px solid ${({ theme }) => theme.primary400};
-
-    div {
-      border-top-left-radius: 2px;
-      border-bottom-left-radius: 2px;
-    }
-  }
-
-  #lastBar {
-    border-top-right-radius: 4px;
-    border-bottom-right-radius: 4px;
-    border-right: 3px solid ${({ theme }) => theme.primary400};
-
-    div {
-      border-top-right-radius: 2px;
-      border-bottom-right-radius: 2px;
-    }
-  }
-
   small {
     font-weight: bold;
     color: ${({ theme }) => theme.paragraph};
   }
 
   .stepProgress {
-    div div {
-      height: 8px;
-      width: 100%;
-      border: 2px solid ${({ theme }) => theme.primary400};
+    div {
       background-color: ${({ theme }) => theme.primary400};
     }
 
@@ -57,12 +32,20 @@ export const Container = styled.div`
 `;
 
 export const Bar = styled.div`
-  width: 100%;
   height: 12px;
   background-color: ${({ theme }) => theme.primary200};
   border-top: 3px solid ${({ theme }) => theme.primary400};
   border-bottom: 3px solid ${({ theme }) => theme.primary400};
 
-  display: flex;
-  align-items: center;
+  &#firstBar {
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+    border-left: 3px solid ${({ theme }) => theme.primary400};
+  }
+
+  &#lastBar {
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border-right: 3px solid ${({ theme }) => theme.primary400};
+  }
 `;
